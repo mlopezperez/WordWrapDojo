30547b7de8ed9d26d97b051e8f9889c2e3cada7a
rreina <ruben.reina.dev@gmail.com>
2018-02-21 20:37:34 +0100

    Implement initial WordWrapper

---
+++ CodingDojo.WordWrap/WordWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CodingDojo.WordWrap
+{
+    public class WordWrapper: IWrapper
+    {
+        public string Wrap(string input, int columns)
+        {
+            if (String.IsNullOrEmpty(input)) 
+            {
+                throw new ArgumentNullException();
+            }
+            return input;
+        }
+    }
+}
